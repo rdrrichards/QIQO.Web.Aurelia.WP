ef5791dbc7830f77db98ec0264ce1629bc1311f0
rdrrichards@hotmail.com <rdrrichards@hotmail.com>
2017-02-21 19:29:37 -0600

    a little clean up; edit mv changes

--- src/products/product.edit.js
+++ src/products/product.edit.js
@@ -2,26 +2,29 @@ import { ProductService } from '../services/product.service';
 import { inject } from 'aurelia-framework';
 
 @inject(ProductService)
-export class ProductEdit{
-    constructor(productService){
-        this.pageTitle = "Edit Product";
-        this._productService = productService;
-    }
-    
-    activate(params, routeConfig, navigationInstruction){
-		this.router = navigationInstruction.router;
-        this._productService.getProduct(params.id)
-            .then(product => this.product = product);
-    }
-    
-    save(){
-        this._productService.updateProduct(this.product)
-            .then(product => this.router.navigateToRoute('products'));
-    }
-    
-    delete(){
-        this._productService.deleteProduct(this.product.productKey)
-            .then(product => this.router.navigateToRoute('products'));
-            // really just need to do a redirect here (on success)
-    }
+export class ProductEdit {
+  constructor(productService) {
+    this.pageTitle = "Edit Product";
+    this._productService = productService;
+  }
+
+  activate(params, routeConfig, navigationInstruction) {
+    this.router = navigationInstruction.router;
+    this._productService.getProduct(params.id)
+      .then((product) => {
+        this.product = product;
+        this.editProduct = product;
+      });
+  }
+
+  save() {
+    this._productService.updateProduct(this.editProduct)
+      .then(product => this.router.navigateToRoute('products'));
+  }
+
+  delete() {
+    this._productService.deleteProduct(this.product.productKey)
+      .then(product => this.router.navigateToRoute('products'));
+    // really just need to do a redirect here (on success)
+  }
 }
